e813a58fa37f53abafb5dd738e1d1e27b70c5dce
andyska09 <andyska09@gmail.com>
2017-08-01 23:24:45 +0200

    first samples

---
+++ samples/led-touch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+# from ev3dev.ev3 import *
+
+print("jedeme 123")
+# ts = TouchSensor()
+# # while True:
+# Leds.set_color(Leds.LEFT, (Leds.GREEN, Leds.RED)[ts.value()])
+
+# do not delete lines below
+# export PATH=/usr/bin/python3:$PATH
+# https://winsmarts.com/new-line-characters-in-text-files-between-mac-and-windows-3695b7beb685
+# perl -pi -e 's/\r\n|\n|\r/\n/g'   file-to-convert
