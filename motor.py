2e30baf891baaa8adadcceee00f1b52c85129f44
andyska09 <andyska09@gmail.com>
2017-08-03 12:34:18 +0200

    new motor

--- motor.py
+++ motor.py
@@ -2,7 +2,13 @@
 
 
 from ev3dev.ev3 import *
-# import ev3dev.ev3 as ev3
+from time import sleep
 
 m = LargeMotor('outB')
-m.run_timed(time_sp=3000, speed_sp=500)
+
+m.run_to_rel_pos(position_sp=280, speed_sp=1000, stop_action="hold")
+
+sleep(5)   # Give the motor time to move
+
+#m = LargeMotor('outB')
+#m.run_timed(time_sp=3000, speed_sp=500)
