9d8bac0feba0463244101cdad0612f9563b18bb6
andyska09 <andyska09@gmail.com>
2018-01-17 19:14:30 +0100

    testing motors

--- motor-time.py
+++ motor-time.py
@@ -3,9 +3,12 @@
 
 from ev3dev.ev3 import *
 from time import sleep
-m = LargeMotor('outB')
-m.run_timed(time_sp=3000, speed_sp=-750)
-print("set speed (speed_sp) = " + str(m.speed_sp))
+m1 = LargeMotor('outC')
+m1.run_timed(time_sp=3000, speed_sp=-750)
+m2 = LargeMotor('outB')
+m2.run_timed(time_sp=3000, speed_sp=-750)
+
+# print("set speed (speed_sp) = " + str(m1.speed_sp))
 sleep(1)  # it takes a moment for the motor to start moving
-print("actual speed = " + str(m.speed))
+# print("actual speed = " + str(m1.speed))
 sleep(5)
