8fc0f6678036a3c7be7215c1b84b624718a5162a
andyska09 <andyska09@gmail.com>
2018-02-03 11:16:35 +0100

    test button

--- brick-buttons.py
+++ brick-buttons.py
@@ -1,11 +1,10 @@
-
 #!/usr/bin/env python3
 
 from ev3dev.ev3 import *
 from time import sleep
 
 btn = Button()
-
-while not btn.backspace:
-    print(btn.left)
-    sleep(1)
+i = 0
+while not btn.backspace and i < 10000:
+    print(i)
+    i += 1
