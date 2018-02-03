51ab605adc0fb2ac3e8b8e7cb6e9038d0ff90885
andyska09 <andyska09@gmail.com>
2018-02-03 11:16:19 +0100

    counting oscillations

--- line-follower-2-.py
+++ line-follower-2-.py
@@ -4,7 +4,6 @@
 
 from ev3dev.ev3 import *
 from time import sleep
-import time
 from datetime import datetime
 
 cl_left = ColorSensor("in1")  # left cl sensor
@@ -13,13 +12,13 @@ cl_right = ColorSensor("in3")  # right cl sensor
 ts = TouchSensor()
 us = UltrasonicSensor()
 
-# us.mode = 'US-DIST-CM'
+us.mode = 'US-DIST-CM'
 cl_left.mode = 'COL-REFLECT'
 cl_middle.mode = 'COL-REFLECT'
 cl_right.mode = 'COL-REFLECT'
 assert ColorSensor().connected, "Connect a color sensor to any sensor port"
-assert ts.connected, "Connect a touch sensor to any sensor port"
-# assert us.connected, "Connect a single US sensor to any sensor port"
+# assert ts.connected, "Connect a touch sensor to any sensor port"
+assert us.connected, "Connect a single US sensor to any sensor port"
 
 m_b = LargeMotor('outB')
 m_c = LargeMotor('outC')
@@ -32,17 +31,12 @@ class LineFollower:
 
     def __init__(self, colors_file_name, kp, ki, kd):
         with open('color.txt') as f:
-            white = int(f.readline())
-            print(white)
-            black = int(f.readline())
-            print(black)
-        self.target = (white - black) / 2 + black
-        self.tp = 450
-        self.kp = 1.020
-        self.ki = 0.037
-        self.kd = 7.108
-        print("Robot is initialized with values:\n\ttarget = %s\n\tk_d = %s" %
-              (self.target, self.kd))
+            self.white = int(f.readline())
+            print(self.white)
+            self.black = int(f.readline())
+            print(self.black)
+        self.target = (self.white - self.black) / 2 + self.black
+        print("target = %s" % (self.target))
 
     def write_log(self, log):
         f = open("lightlog.txt","w")
@@ -50,65 +44,86 @@ class LineFollower:
         f.close()
 
     def sleduj_caru(self):
-        print("\n** this is method.sleduj_caru(s*")
-        on_side = "left"
+        target = self.target
+        tp = 450
+        kp = 2.3
+        ki = 0
+        kd = 0
         integral = 0
         last_err = 0
         i = 0
+        prev_sign = None
+        oscil_count = 0
+        on_side = "L"
         side_was_changed = False 
-
-        log = ""
-        self.t_start = time.time()
+        log_line = ""
+        print("start")
+        t_start = time.time()
         # Stop program by pressing touch sensor button
-        while (not ts.value()) and i < 800: #((time.time() - self.t_start) < 10) #or (us.value() > 300)):
-            # log_line = "%4d" % i
-            cleft = cl_left.value()
+        # (not ts.value()) and
+        while (((time.time() - t_start) < 10) or (us.value() > 300)) and (i < 1000):
+            cright = 0
             cmiddle = cl_middle.value()
-            cright = cl_right.value()
+            cleft = 0
             # if side_was_changed:
             #     if (cmiddle > 25) and (cmiddle < 75):
             #         side_was_changed = False
             # else:
-            #     if (on_side == "left"):
-            #         if (cmiddle > 80) and (cright > 80):
-            #             on_side = "right"
-            #             side_was_changed = True
+            #     if (on_side == "L"):
+            #         if (cmiddle > 80):
+            #             cright = cl_right.value()
+            #             if  (cright > 80):
+            #                 on_side = "R"
+            #                 side_was_changed = True
             #     else:
-            #         if (cmiddle > 80) and (cleft > 80):
-            #             on_side = "left"
-            #             side_was_changed = True
-    
-            # log_line += " %s %s %3d %3d %3d \n" % (on_side, side_was_changed, cleft, cmiddle, cright)
-
-            err = self.target - cmiddle
-            integral = err + integral
+            #         if (cmiddle > 80):
+            #             cleft = cl_left.value()
+            #             if (cleft > 80):
+            #                 on_side = "L"
+            #                 side_was_changed = True
+            err = target - cmiddle
+            integral = err + ((2/3)*integral)
             # This is pid formula
-            corr = err * self.kp + integral * \
-                self.ki + (err - last_err) * self.kd
-
-            # if on_side == "right":
-                # corr = -corr 
-
-            tp_r = self.tp + (corr * 10) / 2  # this is motor on outB
-            tp_l = self.tp - (corr * 10) / 2  # this is motor on outC
-            tp_r = min(1000, max(-1000, tp_r))
-            tp_l = min(1000, max(-1000, tp_l))
+            corr = (err * kp + integral * ki + (err - last_err) * kd) * 5
+            # corr = corr * 5  # (corr * 10) / 2
+            if on_side == "R":
+                corr = -corr 
+            tp_r = tp + corr  # this is motor on outB
+            tp_l = tp - corr  # this is motor on outC
+            if (tp_r > 1000):
+                tp_r = 1000
+            elif (tp_r < -1000):
+                tp_r = -1000
+            if (tp_l > 1000):
+                tp_l = 1000    
+            elif (tp_l < -1000):
+                tp_l = -1000
             m_b.run_forever(speed_sp=tp_r)  # This will makes robot turning
             m_c.run_forever(speed_sp=tp_l)
             last_err = err
-            i = i + 1
-            # log = log + log_line
-        log_line = "duration: %s sec" % str(time.time() - self.t_start)
-        log += log_line
-        self.write_log(log)
-        print(log_line)
+            # log_line += " %s %s %3d %3d %3d \n" % (on_side, side_was_changed, cleft, cmiddle, cright)
+            if prev_sign is None:
+                if err > 0:
+                    prev_sign = 1
+                else:
+                    prev_sign = -1
+            else:
+                if prev_sign == 1 and err < 0:
+                    oscil_count += 1
+                elif prev_sign == -1 and err > 0:
+                    oscil_count += 1 
+                if err > 0:
+                    prev_sign = 1
+                else:
+                    prev_sign = -1                   
+            log_line += " %s %s %3d %3d \n" % (str(prev_sign), err> 0, oscil_count, err)
+            i += 1 
+        print("duration: %s sec" % str(time.time() - t_start))
+        self.write_log(log_line)
+
         m_b.stop(stop_action="hold")
         m_c.stop(stop_action="hold")
 
 
-
-print("Getting started with objects\n----------------------------\n")
-print("1")
 lf = LineFollower('color.txt', 1.3, 0, 0)
-print("ki = ", lf.ki)
 lf.sleduj_caru()
