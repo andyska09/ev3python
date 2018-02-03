275c5ec042ab5a0b6456ce0a0265e6f63fc2c43c
andyska09 <andyska09@gmail.com>
2018-02-03 11:16:52 +0100

    using button to stop

--- sen-col-calib.py
+++ sen-col-calib.py
@@ -4,22 +4,22 @@
 from ev3dev.ev3 import *
 from time import sleep
 
-def measure_and_save_color(say_prompt, touch_sensor, cl, file):
+def measure_and_save_color(say_prompt, btn, cl, file):
     Sound.speak(say_prompt).wait()
     color = cl.value()
-    while not touch_sensor.value():
+    while not btn.backspace:
         color = cl.value()
     print(color)
     text_file.write("%s\n" % color)
     return
 
 text_file = open("color.txt", "w")
-ts = TouchSensor()
+btn = Button()
 cl = ColorSensor()
 assert cl.connected, "Connect a color sensor to any sensor port"
 cl.mode = 'COL-REFLECT'
 
-measure_and_save_color('Show me white', ts, cl, text_file)
-measure_and_save_color('Show me black', ts, cl, text_file)
+measure_and_save_color('Show me white', btn, cl, text_file)
+measure_and_save_color('Show me black', btn, cl, text_file)
 
 text_file.close()
