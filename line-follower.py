76e38b332c21d3454c758564772689ad7ba4ca7b
andyska09 <andyska09@gmail.com>
2018-02-03 18:37:55 +0100

    correction of correction

--- line-follower.py
+++ line-follower.py
@@ -46,10 +46,13 @@ class LineFollower:
 
     def sleduj_caru(self):
         target = self.target
-        tp = 450
-        kp = 1.380
-        ki = 0.073
-        kd = 6.523
+        tp = 500
+        # kp = 6.818
+        # ki = 0.347
+        # kd = 33.461
+        kp = 9.818
+        ki = 0.347
+        kd = 33.461
         integral = 0
         last_err = 0
         i = 0
@@ -61,55 +64,83 @@ class LineFollower:
         # Stop program by pressing touch sensor button
         # (not ts.value()) and
         # 
-        while (((time.time() - t_start) < 10) or (us.value() > 300)) \
-            and ((i % 11 != 0) or (not btn.backspace)) \
-            and (i < 1000):
+        # while ( \
+        #     and ((i % 11 != 0) or (not btn.backspace)):        
+        #((i % 7 != 0) or (not btn.backspace and \
+        while i < 700:
             cright = 0
             cmiddle = cl_middle.value()
             cleft = 0
-            # if side_was_changed:
-            #     if (cmiddle > 25) and (cmiddle < 75):
-            #         side_was_changed = False
-            # else:
-            #     if (on_side == "L"):
-            #         if (cmiddle > 80):
-            #             cright = cl_right.value()
-            #             if  (cright > 80):
-            #                 on_side = "R"
-            #                 side_was_changed = True
-            #     else:
-            #         if (cmiddle > 80):
-            #             cleft = cl_left.value()
-            #             if (cleft > 80):
-            #                 on_side = "L"
-            #                 side_was_changed = True
+            
+            if side_was_changed:
+                if (cmiddle > 25) and (cmiddle < 75):
+                    side_was_changed = False
+            else:
+                if (on_side == "L"):
+                    if (cmiddle > 90):
+                        cright = cl_right.value()
+                        if  (cright > 90):
+                            on_side = "R"
+                            side_was_changed = True
+                else:
+                    if (cmiddle > 90):
+                        cleft = cl_left.value()
+                        if (cleft > 90):
+                            on_side = "L"
+                            side_was_changed = True
             err = target - cmiddle
             integral = err + ((2/3)*integral)
             # This is pid formula
-            corr = (err * kp + integral * ki + (err - last_err) * kd) * 5
+            corr_p = (err * kp)
+            corr_i = (integral * ki)
+            corr_d = ((err - last_err) * kd)
+            corr = (corr_p + corr_i +corr_d) / 2
             # corr = corr * 5  # (corr * 10) / 2
             if on_side == "R":
                 corr = -corr 
             tp_r = tp + corr  # this is motor on outB
             tp_l = tp - corr  # this is motor on outC
+            tp_rc = tp_r
+            tp_lc = tp_l
+            cut_r = 0
+            cut_l = 0
             if (tp_r > 1000):
-                tp_r = 1000
+                cut_r = tp_r - 1000
+                # tp_r = 1000
+                tp_rc = 1000
+                tp_lc = tp_l - cut_r
+                tp_lc = min(1000, max(-1000, tp_lc))                
             elif (tp_r < -1000):
-                tp_r = -1000
+                cut_r = tp_r + 1000 
+                # tp_r = -1000
+                tp_rc = -1000
+                tp_lc = tp_l - cut_r
+                tp_lc = min(1000, max(-1000, tp_lc))
             if (tp_l > 1000):
-                tp_l = 1000    
+                cut_l = tp_l - 1000
+                # tp_l = 1000   
+                tp_lc = 1000   
+                tp_rc = tp_r - cut_l
+                tp_rc = min(1000, max(-1000, tp_rc))                
             elif (tp_l < -1000):
-                tp_l = -1000
-            m_b.run_forever(speed_sp=tp_r)  # This will makes robot turning
-            m_c.run_forever(speed_sp=tp_l)
+                cut_l = tp_l + 1000 
+                # tp_l = -1000
+                tp_lc = -1000
+                tp_rc = tp_r - cut_l
+                tp_rc = min(1000, max(-1000, tp_rc))
+            m_b.run_forever(speed_sp=tp_rc)  # This will makes robot turning
+            m_c.run_forever(speed_sp=tp_lc)
             last_err = err
-            # log_line += " %s %s %3d %3d %3d \n" % (on_side, side_was_changed, cleft, cmiddle, cright)
+            #  log_line += " %s %s %3d %3d %3d \n" % (on_side, side_was_changed, cleft, cmiddle, cright)
+            # if (cut_r != 0) or (cut_l != 0):
+                # log_line += " %3d % 5.3f % 5.3f % 5.3f % 5.3f % 5.3f % 5.3f\n" % (
+            # i, tp_l,  tp_r, tp_lc, tp_rc, cut_l, cut_r)
             i += 1 
         print("duration: %s sec" % str(time.time() - t_start))
         self.write_log(log_line)
 
-        m_b.stop(stop_action="hold")
-        m_c.stop(stop_action="hold")
+        m_b.stop(stop_action="brake")
+        m_c.stop(stop_action="brake")
 
 
 lf = LineFollower('color.txt', 1.3, 0, 0)
